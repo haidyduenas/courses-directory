df161d5a7c9b3010a60364760ef6916185cd233b
haidy <haidy>
2017-10-09 14:33:55 -0500

    home

--- src/App.js
+++ src/App.js
@@ -287,7 +287,7 @@ const App = () => (
          <Route path="/teachers" component={Teachers}/> 
          <Route path="/courses" component={Courses}/> 
          
-         <Route component={NotFoundPage}/>
+         <Route component={Home}/>
       </Switch>
     </div>
   </BrowserRouter>
